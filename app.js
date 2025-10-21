85b318d4fb4dfa3dd8281823be3fe75047c93d69
Saikopasu sus <lolololololikovich@gmail.com>
2025-10-21 11:27:53 +0300

    fix: rename classname in app.js to make edit button work

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("editMode");
+    var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .editmode
     if(containsClass){
 
